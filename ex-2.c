28a3a8c97171b79737a487b2ccde0711a563540b
Antony rubombora <tonyrubombora@gmail.com>
2022-01-02 16:37:44 +0300

    Commit ex 2

---
+++ ex-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Run the damn code");
+    return 0;
+	// FILE* in_file = fopen("output", "r");
+
+    // if (in_file == NULL || out_file == NULL)
+    // {
+    //     printf("Error! Could not open file\n");
+    //     exit(-1); // must include stdlib.h 
+    // }
+
+    // printf("this is a test %d\n", integer); // write to screen
+}
