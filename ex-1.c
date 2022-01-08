e8d81b4ea09b7fd10cc6f7979b117f8adab83ae1
Antony rubombora <tonyrubombora@gmail.com>
2022-01-08 16:32:26 +0300

    minus signs included

--- ex-1.c
+++ ex-1.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
 int main() {
-	int in;
-	int temp;
-	int a = 0;
+    
+	int in, temp, a = 0;
 
 	printf("Enter an integer: ");
 	scanf("%d", &in);
 
-	while ((in / 10) > 0.1 || (in / 10) == 0.1) {
+    if(in > 0){
+        while ((in / 10) > 0.1 || (in / 10) == 0.1) {
 		temp = in / 10;
 		a++;
 		in = temp;
 	}
+    } else {
+        while ((in / 10) < -0.1 || (in / 10) == -0.1) {
+		temp = in / 10;
+		a++;
+		in = temp;
+	}
+    }
+    
 	printf("%d", a + 1);
 	return 0;
 }
